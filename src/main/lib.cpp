9eb353d44b06a0eac694c1b29e5f290c7cde7c08
Sven Lukas <Sven.Lukas@googlemail.com>
2021-08-01 19:12:10 +0200

    Fixed new module interface

--- src/main/lib.cpp
+++ src/main/lib.cpp
@@ -1,6 +1,6 @@
 #include <curl4esl/Module.h>
 
-#include <esl/Module.h>
+#include <esl/module/Module.h>
 
 extern "C" void esl__module__library__install(esl::module::Module* module) {
 	if(module != nullptr) {
